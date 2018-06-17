f548f36d838e8f58ce65972c85d7b0d4a645439e
sanjeev <sanjeev@sanjeev-Dell13>
2018-06-16 19:56:32 -0500

    Project Done

---
+++ JenkinProject/src/testcases/TC_003.java
@@ -0,0 +1,23 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_003 {
+	
+	@Test
+	public void tc_001()
+	{
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		
+		driver.findElement(By.id("email")).sendKeys("test1");
+		driver.findElement(By.id("pass")).sendKeys("pass");
+		driver.quit();
+		
+	}
+
+}
